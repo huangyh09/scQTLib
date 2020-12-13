a3797633d3d0e170e1bfb6658352e07ee6c9fdf0
Yuanhua Huang <huangyh094@gmail.com>
2020-12-13 14:15:31 +0000

    support multiple processes

--- scqtlib/stats/qtl.py
+++ scqtlib/stats/qtl.py
@@ -1,40 +1,114 @@
 import limix
 import numpy as np
+import multiprocessing
+from functools import partial
 from statsmodels.sandbox.stats.multicomp import multipletests
+
+from scipy.stats import itemfreq
 
 from .glm import glm_LRT
 
-def qtl_scan(y, GT, depth, M=None, interact=None, method='NB', **kwargs):
+def show_progress(RV=None):
+    return RV
+
+def qtl_scan(y, GT, depth, M=None, interact=None, method='limix', nproc=1, 
+             min_AF=0.05, **kwargs):
     n_gene = y.shape[1]
     n_sample = y.shape[0]
     
     pval_GT = np.array([None] * y.shape[1], float)
     fdr_GT  = np.array([None] * y.shape[1], float)
+    coef_GT = np.array([None] * y.shape[1], float)
+    coef_GT_se = np.array([None] * y.shape[1], float)
+    
     pval_inter = np.array([None] * y.shape[1], float)
     fdr_inter  = np.array([None] * y.shape[1], float)
+    coef_inter = np.array([None] * y.shape[1], float)
+    coef_inter_se = np.array([None] * y.shape[1], float)
     
-    for i in range(y.shape[1]): # SNP-gene pair
-        if np.max(np.unique(GT[:, i], return_counts=True)[1]) > (0.95 * n_sample):            
-            continue
+    ## define function with kwargs
+    if method == 'statsmodels' or method == 'NB':
+        func_use = partial(qtl_statsmodels, **kwargs)
+    else:
+        func_use = partial(qtl_limix, **kwargs)
+        
+    ## run GLM with single or multiple processes
+    if nproc == 1:
+        for i in range(y.shape[1]): # SNP-gene pair
+            if np.max(itemfreq(GT[:, i])[:, 1]) > ((1 - min_AF) * n_sample):
+                continue
+                
+            pval_GT[i], pval_inter[i], _qtl1, _qtl2 = func_use(
+                y[:, i], GT[:, i], depth, M, interact)
             
-        if method == 'NB':
-            pval_GT[i], pval_inter[i] = qtl_NB(
-                y[:, i], GT[:, i], depth, M, interact, **kwargs)[:2]
-        else:
-            pval_GT[i], pval_inter[i] = qtl_limix(
-                y[:, i], GT[:, i], depth, M, interact, **kwargs)[:2]
-
+            if method != "limix":
+                continue
+            
+            coef_GT[i] = np.array(_qtl1.effsizes['h2']['effsize'])[-1]
+            coef_GT_se[i] = np.array(_qtl1.effsizes['h2']['effsize_se'])[-1]
+            coef_inter[i] = np.array(_qtl2.effsizes['h2']['effsize'])[-1]
+            coef_inter_se[i] = np.array(_qtl2.effsizes['h2']['effsize_se'])[-1]
+    else:
+        pool = multiprocessing.Pool(processes=nproc)
+        result = []
+        idx_kept = []
+        for i in range(y.shape[1]): # SNP-gene pair
+            if np.max(itemfreq(GT[:, i])[:, 1]) > ((1 - min_AF) * n_sample):
+                continue
+                
+            idx_kept.append(i)
+            result.append(pool.apply_async(
+                func_use, (y[:, i], GT[:, i], depth, M, interact),
+                callback=show_progress
+            ))
+        
+        pool.close()
+        pool.join()
+        result = [res.get() for res in result]
+        
+        for ii in range(len(idx_kept)):
+            _idx = idx_kept[ii]
+            pval_GT[_idx], pval_inter[_idx], _qtl1, _qtl2 = result[ii]
+            
+            if method != "limix":
+                continue
+            
+            coef_GT[_idx] = np.array(_qtl1.effsizes['h2']['effsize'])[-1]
+            coef_GT_se[_idx] = np.array(_qtl1.effsizes['h2']['effsize_se'])[-1]
+            coef_inter[_idx] = np.array(_qtl2.effsizes['h2']['effsize'])[-1]
+            coef_inter_se[_idx] = np.array(_qtl2.effsizes['h2']['effsize_se'])[-1]
+            
+    
+    ## Multiple testing correction
     _idx = pval_GT >= 0
     print("%d out %d tests valid for FDR correction" %(sum(_idx), len(_idx)))
     if sum(_idx) > 1:
         fdr_GT[_idx] = multipletests(pval_GT[_idx], method='fdr_bh')[1]
         fdr_inter[_idx] = multipletests(pval_inter[_idx], method='fdr_bh')[1]
 
-    return pval_GT, fdr_GT, pval_inter, fdr_inter
+    ## Return results
+    RV = {}
+    RV['pval_GT'] = pval_GT
+    RV['fdr_GT']  = fdr_GT
+    RV['pval_inter'] = pval_inter
+    RV['fdr_inter']  = fdr_inter
+    
+    RV['coef_GT']  = coef_GT
+    RV['coef_GT_se']  = coef_GT_se
+    RV['coef_inter']  = coef_inter
+    RV['coef_inter_se']  = coef_inter_se
+    
+    return RV
 
 
 def qtl_NB(y, GT, depth, M=None, interact=None, **kwargs):
+    print("Please use `qtl_statsmodels` in future.")
+    return qtl_statsmodels(y, GT, depth, M=None, interact=None, **kwargs)
+
+
+def qtl_statsmodels(y, GT, depth, M=None, interact=None, **kwargs):
     """
+    default: family=sm.families.NegativeBinomial()
     """
     if M is not None:
         M1 = np.append(np.log(depth + 1).reshape(-1, 1), M, axis=1)
