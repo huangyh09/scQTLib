25e04b6a10d06fd9ba0e7a9a0387aef6eea590b9
Yuanhua Huang <huangyh094@gmail.com>
2021-04-20 15:46:29 +0800

    add downsample

--- scqtlib/utils/base_utils.py
+++ scqtlib/utils/base_utils.py
@@ -57,3 +57,63 @@ def match(ref_ids, new_ids, uniq_ref_only=True):
     origin_idx = np.argsort(RT_idx1)
     RT_idx = np.array(RT_idx2)[origin_idx]
     return RT_idx
+
+
+def downsample(labels, out_count, balanced_out=True):
+    """Subset a list of labels to a specific number
+    
+    Parameters
+    ----------
+    labels: list or numpy.array
+        A label list to down sample
+    out_count: int
+        The count of samples to return
+    balanced_out: bool
+        If True, keep each group as balanced. In the mode,
+        a cutoff will detected to retain the small groups,
+        and the largest group may have a few sample more than
+        others to fill the gap till out_count.
+        If False, a sample numpy.random.choice is in use.
+        
+    Return
+    ------
+    The index of labels that are kept.
+    """
+    if balanced_out:
+        if type(labels) is list:
+            labels = np.array(labels)
+        
+        id_uniq, id_count = np.unique(labels, return_counts=True)
+        
+        # find the min_counts
+        id_uniq_sorted = id_uniq[np.argsort(id_count)]
+        id_count_sorted = np.sort(id_count)
+        
+        ii_next = len(id_count)
+        idx_out = []
+        for i in range(len(id_count)):
+            if id_count_sorted[i] * (len(id_count) - i) < out_count - len(idx_out):
+                idx_matched = np.where(labels == id_uniq_sorted[i])[0]
+                idx_out = np.append(idx_out, idx_matched)
+            else:
+                n_count = int((out_count - len(idx_out)) / (len(id_count) - i))
+                ii_next = i
+                break
+            
+        print("min_count: %d. %d out of %d groups below it" 
+              %(n_count, ii_next, len(id_count)))
+        
+        for i in range(ii_next, len(id_count)):
+            if i == len(id_count) - 1:
+                n_count = out_count - len(idx_out)
+            
+            idx_matched = np.where(labels == id_uniq_sorted[i])[0]
+            idx_sampled = np.random.choice(idx_matched, n_count, replace=False)
+            idx_out = np.append(idx_out, idx_sampled)
+            
+            # print(i, len(idx_matched), id_uniq_sorted[i])
+            
+    else:
+        idx_out = np.random.choice(len(labels), out_count, replace=False)
+                
+    return idx_out.astype(int)
